517e9597e2ae580c5dd2bbbf78a7cd444965da41
prnvsan <pranavhnairxd@gmail.com>
2021-01-31 13:34:37 +0530

    added listing of posts and edit, show, create

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create 
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to post_path(@post)
+    else
+    render 'new'
+    end
+  end
+  
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Post was updated"
+      redirect_to post_path(@post)
+    else
+      flash[:notice] = "Post was not updated"
+      render 'edit'
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :body, presence: true, length: {minimum:10, maximum: 100}
+
+end
